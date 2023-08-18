3e69562d34dea9451efd3b1413f1a967ae61da36
morfeo68 <joseedet@gmail.com>
2023-08-18 17:20:55 +0200

    Context

--- BlogNet.Persistencia/BlogNetContext.cs
+++ BlogNet.Persistencia/BlogNetContext.cs
@@ -7,19 +7,19 @@ namespace BlogNet.Persistencia
 {
     public class BlogNetContext
     {
-        public class BlogNetContext:DbContext
-    {
+        public class BlogNetContext: DbContext
+        {
         public BlogNetContext(DbContextOptions options):base(options)
         {
              
         }      
-        public DbSet <Alamacen> { get; set; }
-        public DbSet <Cliente> { get; set; }
-        public DbSet <Departamento> { get; set; }
-        public DbSet  <IVA> { get; set; }
-        public DbSet  <Medida>{ get; set; }
-        public DbSet <Proveedor>{ get; set; }
-        public DbSet  <TipoDocumento> { get; set; }
+        public DbSet <Alamacen> Almacen { get; set; }
+        public DbSet <Cliente> Cliente { get; set; }
+        public DbSet <Departamento> Departamento { get; set; }
+        public DbSet  <IVA> IVA { get; set; }
+        public DbSet  <Medida> Medida { get; set; }
+        public DbSet <Proveedor> Proveedor{ get; set; }
+        public DbSet  <TipoDocumento> TipoDocumento{ get; set; }
         
     }
     }
